6989294beb2a48643d3cf95e49c72cd28614e8f2
mdctbanglamung <105916138+mdctbanglamung@users.noreply.github.com>
2022-07-04 23:08:55 +0700

    Update dmonitoringd.py

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -5,7 +5,7 @@ import cereal.messaging as messaging
 from selfdrive.controls.lib.events import Events
 from selfdrive.monitoring.driver_monitor import DriverStatus, MAX_TERMINAL_ALERTS, MAX_TERMINAL_DURATION
 from selfdrive.locationd.calibrationd import Calibration
-from selfdrive.monitoring.hands_on_wheel_monitor import HandsOnWheelStatus
+
 
 from common.realtime import DT_DMON, sec_since_boot
 import time
@@ -22,7 +22,7 @@ def dmonitoringd_thread(sm=None, pm=None):
   driver_status.is_rhd_region = Params().get("IsRHD") == b"1"
 
   offroad = Params().get("IsOffroad") == b"1"
-  hands_on_wheel_status = HandsOnWheelStatus()
+  
 
   sm['liveCalibration'].calStatus = Calibration.INVALID
   sm['liveCalibration'].rpyCalib = [0, 0, 0]
@@ -37,7 +37,7 @@ def dmonitoringd_thread(sm=None, pm=None):
   v_cruise_last = 0
   driver_engaged = False
   steering_wheel_engaged = False
-  hands_on_wheel_monitoring_enabled = Params().get("HandsOnWheelMonitoring") == b"1"
+  
 
   # dp
   sm['dragonConf'].dpDriverMonitor = True
@@ -75,10 +75,10 @@ def dmonitoringd_thread(sm=None, pm=None):
       driver_engaged = steering_wheel_engaged or sm['carState'].gasPressed
       if driver_engaged:
         driver_status.update(Events(), True, sm['controlsState'].enabled, sm['carState'].standstill)
-      # Update events and state from hands on wheel monitoring status when steering wheel in engaged
-      if steering_wheel_engaged and hands_on_wheel_monitoring_enabled:
-        hands_on_wheel_status.update(Events(), True, sm['controlsState'].enabled, sm['carState'].vEgo)
-      v_cruise_last = v_cruise
+
+
+
+
 
     if sm.updated['model']:
       driver_status.set_policy(sm['model'])
@@ -94,9 +94,9 @@ def dmonitoringd_thread(sm=None, pm=None):
 
     # Update events from driver state
     driver_status.update(events, driver_engaged, sm['controlsState'].enabled, sm['carState'].standstill)
-    # Update events and state from hands on wheel monitoring status
-    if hands_on_wheel_monitoring_enabled:
-      hands_on_wheel_status.update(events, steering_wheel_engaged, sm['controlsState'].enabled, sm['carState'].vEgo)
+
+
+
 
     # build dMonitoringState packet
     dat = messaging.new_message('dMonitoringState')
@@ -116,7 +116,7 @@ def dmonitoringd_thread(sm=None, pm=None):
       "isLowStd": driver_status.pose.low_std,
       "hiStdCount": driver_status.hi_stds,
       "isPreview": offroad,
-      "handsOnWheelState": hands_on_wheel_status.hands_on_wheel_state,
+
     }
     pm.send('dMonitoringState', dat)
     diff = sec_since_boot() - start_time
